d9f875b22d3feed77cde5c92bc95aab47d8f12a6
eastend-street <suminari.yamada@gmail.com>
2023-03-18 11:15:58 +0900

    feat: implement dto to receive email and password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
-  signup() {
+  constructor(private prisma: PrismaService) {}
+  signup(dto: AuthDto) {
     return { msg: 'I have signed up' };
   }
 
